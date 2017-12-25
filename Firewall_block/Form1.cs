690ec1f31bba2f46670bfcbfafbbbda10a729664
SumOfAllN00bs <discordshards@gmail.com>
2017-12-25 13:23:27 +1300

    Add project files.

---
+++ Firewall_block/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Principal;
+using System.Diagnostics;
+using System.IO;
+
+namespace Firewall_block
+{
+    public partial class Form1 : Form
+    {
+        public bool IsUserAdministrator()
+        {
+            bool isAdmin;
+            try
+            {
+                WindowsIdentity user = WindowsIdentity.GetCurrent();
+                WindowsPrincipal principal = new WindowsPrincipal(user);
+                isAdmin = principal.IsInRole(WindowsBuiltInRole.Administrator);
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                isAdmin = false;
+            }
+            catch (Exception ex)
+            {
+                isAdmin = false;
+            }
+            return isAdmin;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            //if (!IsUserAdministrator())
+            //{
+            //    MessageBox.Show("Need Admin Privileges");
+            //    this.Close();
+            //}
+            this.AllowDrop = true;
+            this.DragEnter += new DragEventHandler(Form1_DragEnter);
+            this.DragDrop += new DragEventHandler(Form1_DragDrop);
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (string file in files)
+            {
+                MessageBox.Show(file);
+                Process process = new Process();
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                //startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                startInfo.WorkingDirectory = @"C:\Windows\System32";
+                startInfo.FileName = "cmd.exe";
+                startInfo.Verb = "runas";
+                startInfo.Arguments = "/user:Administrator \"cmd /K \"netsh advfirewall firewall add rule name=\"" + Path.GetFileNameWithoutExtension(file) + "_block\" dir=in protocol=tcp program=\"" + file + "\" action=block\"" + "\"";
+                process.StartInfo = startInfo;
+                process.Start();
+                //string strCmdText;
+                //strCmdText = "/C netsh advfirewall firewall add rule name=\"%~n1_block\" dir=in protocol=tcp program=%1 action=block";
+                //System.Diagnostics.Process.Start("CMD.exe", strCmdText);
+            }
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+            //MessageBox.Show("Test");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
